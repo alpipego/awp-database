ee02598bd3f77605a89cdd75a4b846e1e68dec75
Alex Goller <alpipego@protonmail.com>
2019-06-04 12:27:58 +0200

    add basic query builder

--- src/Table.php
+++ src/Table.php
@@ -16,6 +16,9 @@ class Table implements TableInterface
 	 * @var string
 	 */
 	protected $table;
+	protected $sort = [];
+	protected $limit = [];
+	protected $where = [];
 
 	public function __construct(string $table, wpdb $db)
 	{
@@ -28,43 +31,90 @@ class Table implements TableInterface
 		return $this->table;
 	}
 
-	public function count(string $where = '1=1') : int
+	public function sort(string $sort)
 	{
-		$limit = PHP_INT_MAX;
+		$this->sort[] = $sort;
 
-		return (int)$this->db->get_var("SELECT count(*) FROM {$this->table} WHERE ${where} LIMIT ${limit};");
+		return $this;
 	}
 
-	public function get_results(string $select = '*', string $where = '1=1') : array
+	public function limit(string $limit)
 	{
-		return $this->db->get_results("SELECT ${select} FROM {$this->table} WHERE ${where};", ARRAY_A);
+		$this->limit[] = $limit;
+
+		return $this;
 	}
 
-	public function get_col(string $colname, string $where = '1=1') : array
+	public function where(array $where)
 	{
-		return $this->db->get_col("SELECT ${colname} FROM {$this->table} WHERE ${where};");
+		$this->where[] = $where;
+
+		return $this;
 	}
 
-	public function get_cols(string $colnames, string $where = '1=1') : array
+	public function count(array $where = []) : int
 	{
-		return $this->db->get_results("SELECT ${colnames} FROM {$this->table} WHERE ${where};", ARRAY_A);
+		// "SELECT * FROM {$this->table} WHERE $where;"
+		return (int)$this->db->get_var($this->query([
+			'fields' => ['count(*)'],
+			'where'  => $this->parseWhere($where),
+			'limit'  => PHP_INT_MAX,
+		]));
 	}
 
-	public function get_row(string $where) : array
+	public function get_results(array $fields = ['*'], array $where = [], int $limit = null) : array
 	{
-		return $this->db->get_row("SELECT * FROM {$this->table} WHERE ${where};", ARRAY_A);
+		// "SELECT ${fields} FROM {$this->table} WHERE ${where} LIMIT ${limit};"
+		return $this->db->get_results([
+				'fields' => $fields,
+				'where'  => $this->parseWhere($where),
+				'limit'  => is_null($limit) ? '' : ' LIMIT ' . $limit,
+			], ARRAY_A) ?? [];
 	}
 
-	public function get_rows(string $where = '1=1', int $limit = null) : array
+	public function get_col(string $field, array $where = []) : array
 	{
-		$limit = is_null($limit) ? '' : ' LIMIT ' . $limit;
-
-		return $this->db->get_results("SELECT * FROM {$this->table} WHERE ${where}${limit};", ARRAY_A);
+		// "SELECT ${field} FROM {$this->table} WHERE ${where};"
+		return $this->db->get_col([
+				'fields' => [$field],
+				'where'  => $this->parseWhere($where),
+			]) ?? [];
 	}
 
-	public function get_var(string $field, string $where)
+	public function get_cols(array $fields, array $where = []) : array
 	{
-		return $this->db->get_var("SELECT ${field} FROM {$this->table} WHERE ${where};");
+		// "SELECT ${fields} FROM {$this->table} WHERE ${where};"
+		return $this->db->get_results([
+				'fields' => $fields,
+				'where'  => $this->parseWhere($where),
+			], ARRAY_A) ?? [];
+	}
+
+	public function get_row(array $where) : array
+	{
+		// "SELECT * FROM {$this->table} WHERE ${where};"
+		return $this->db->get_row($this->query([
+				'where' => $this->parseWhere($where),
+			]), ARRAY_A) ?? [];
+
+	}
+
+	public function get_rows(array $where = [], int $limit = null) : array
+	{
+		// "SELECT * FROM {$this->table} WHERE ${where} LIMIT ${limit};"
+		return $this->db->get_results($this->query([
+				'where' => $this->parseWhere($where),
+				'limit' => is_null($limit) ? '' : ' LIMIT ' . $limit,
+			]), ARRAY_A) ?? [];
+	}
+
+	public function get_var(string $field, array $where)
+	{
+		// "SELECT ${field} FROM {$this->table} WHERE ${where};"
+		return $this->db->get_var([
+			'fields' => [$field],
+			'where'  => $this->parseWhere($where),
+		]);
 	}
 
 	public function insert(array $data, array $format = null)
@@ -85,5 +135,104 @@ class Table implements TableInterface
 	public function delete(array $where, array $format)
 	{
 		return $this->db->delete($this->table, $where, $format);
+	}
+
+	public function query(array $args = [])
+	{
+		/**
+		 * @var string $distinct
+		 * @var array $fields
+		 * @var string $join
+		 * @var string $where
+		 * @var string $groupby
+		 * @var string $orderby
+		 * @var string $limit
+		 */
+		extract(array_merge([
+			'distinct' => '',
+			'fields'   => ['*'],
+			'join'     => '',
+			'where'    => '',
+			'groupby'  => '',
+			'orderby'  => '',
+			'limit'    => '',
+		], $args));
+
+		$fields = implode(', ', $fields);
+		$where  = ! empty($where) ? $where : '1=1';
+
+		$query = "SELECT ${distinct} ${fields} FROM {$this->table} ${join} WHERE ${where} ${groupby} ${orderby} ${limit}";
+
+		return $query;
+	}
+
+	protected function parseWhere(array $where) : string
+	{
+		if (empty(array_filter($where, 'is_array'))) {
+			$parsed   = [];
+			$parsed[] = [
+				'field'   => $where[0],
+				'value'   => $where[1],
+				'compare' => $where[2] ?? '=',
+			];
+			$where    = $parsed;
+		}
+
+		if ( ! empty($this->where)) {
+			array_push($where, $this->where);
+		}
+
+		$parser      = function ($chunk) {
+			$field   = $chunk['field'] ?? $chunk[0] ?? '';
+			$value   = $chunk['value'] ?? $chunk[1] ?? '';
+			$compare = $chunk['compare'] ?? $chunk[2] ?? '=';
+			if (empty($field) || empty($value)) {
+				return false;
+			}
+			$type = '%s';
+			if (is_int($value)) {
+				$type = '%d';
+			}
+			if (is_float($value)) {
+				$type = '%F';
+			}
+
+			return $this->db->prepare($field . ' ' . $compare . ' ' . $type, $value);
+		};
+		$whereString = '';
+		$conditions  = array_filter($where, 'is_array');
+		$outer       = 1;
+		$outerCount  = count($conditions);
+		foreach ($conditions as $chunk) {
+			$relation = $this->parseRelation($where['relation']);
+			if ( ! empty(array_filter($chunk, 'is_array'))) {
+				$whereString   .= '(';
+				$innerRelation = $this->parseRelation($chunk['relation']);
+				$inner         = 1;
+				$innerCount    = count($chunk);
+				foreach ($chunk as $item) {
+					$whereString .= $parser($item);
+					if ($innerCount !== $inner++) {
+						$whereString .= sprintf(' %s ', $innerRelation);
+					}
+				}
+				$whereString .= ')';
+				continue;
+			}
+
+			$whereString .= $parser($chunk);
+			if ($outerCount !== $outer++) {
+				$whereString .= sprintf(' %s ', $relation);
+			}
+		}
+
+		return $whereString;
+	}
+
+	private function parseRelation(string $relation = null) : string
+	{
+		$relation = strtoupper((string)$relation);
+
+		return in_array($relation, ['AND', 'OR']) ? $relation : 'AND';
 	}
 }
--- src/TableInterface.php
+++ src/TableInterface.php
@@ -6,19 +6,19 @@ interface TableInterface
 {
 	public function table() : string;
 
-	public function count(string $where = '1=1') : int;
+	public function count(array $where = []) : int;
 
-	public function get_results(string $select = '*', string $where = '1=1') : array;
+	public function get_results(array $fields = ['*'], array $where = [], int $limit = null) : array;
 
-	public function get_col(string $colname, string $where = '1=1') : array;
+	public function get_col(string $field, array $where = []) : array;
 
-	public function get_cols(string $colnames, string $where = '1=1') : array;
+	public function get_cols(array $fields, array $where = []) : array;
 
-	public function get_row(string $where) : array;
+	public function get_row(array $where) : array;
 
-	public function get_rows(string $where = '1=1', int $limit = null) : array;
+	public function get_rows(array $where = [], int $limit = null) : array;
 
-	public function get_var(string $field, string $where);
+	public function get_var(string $field, array $where);
 
 	public function insert(array $data, array $format = null);
 
@@ -27,4 +27,6 @@ interface TableInterface
 	public function update(array $data, array $where, array $format = null, array $whereFormat = null);
 
 	public function delete(array $where, array $format);
+
+	public function query(array $args = []);
 }
